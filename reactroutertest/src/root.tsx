bfa59345d055c5443aed26ce2160cf90ea3ffd80
TomBuilder <47461660+TomBuilder@users.noreply.github.com>
2023-08-18 11:45:45 +0200

    Neue Route

--- reactroutertest/src/root.tsx
+++ reactroutertest/src/root.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Outlet, NavLink, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const Root: React.FC = () => {
    const navigate = useNavigate();
+
    const routeWorkflowClick = () => {
       navigate("/workflow");
+   };
+
+   const routeSettingsClick = () => {
+      navigate("/settings");
    };
 
    return (
       <>
          <p>Die Hauptseite mit Navigation</p>
-         <NavLink to="/workflow">Workflow</NavLink>
          <button type='button' onClick={routeWorkflowClick}>Workflow</button>
+         <button type='button' onClick={routeSettingsClick}>Einstellungen</button>
          <Outlet />
          <p>Fusszeile</p>
       </>);
